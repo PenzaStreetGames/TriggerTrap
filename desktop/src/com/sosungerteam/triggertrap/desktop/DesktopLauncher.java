075c6aef48419fc912fd9da85555f61e9888564b
Pavel Solomatin <crave_ozer_man@mail.ru>
2021-05-01 20:04:01 +0300

    01.05.2021 Начало

---
+++ desktop/src/com/sosungerteam/triggertrap/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.sosungerteam.triggertrap.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.sosungerteam.triggertrap.TriggerTrap;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new TriggerTrap(), config);
+	}
+}
