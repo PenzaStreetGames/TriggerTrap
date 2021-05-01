e054d0ee31254f20c37a7515a560edfd57eeeccf
Mickey327 <usp2002@mail.ru>
2021-05-01 23:13:40 +0300

    Begin of great game(main menu music added).

--- core/src/com/sosungersteam/triggertrap/TriggerTrap.java
+++ core/src/com/sosungersteam/triggertrap/TriggerTrap.java
@@ -1,6 +1,8 @@
 package com.sosungersteam.triggertrap;
 
 import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.audio.Music;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.utils.ScreenUtils;
@@ -8,11 +10,16 @@ import com.badlogic.gdx.utils.ScreenUtils;
 public class TriggerTrap extends ApplicationAdapter {
 	SpriteBatch batch;
 	Texture img;
-	
+	private Music mainmenu;
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
 		img = new Texture("badlogic.jpg");
+		mainmenu = Gdx.audio.newMusic(Gdx.files.internal("Nitro Fun - Cheat Codes.mp3"));
+		mainmenu.play();
+		mainmenu.setLooping(true);
+		mainmenu.setVolume(0.01f);
+
 	}
 
 	@Override
