fb6e6ce1e40aa2e1f73c8a0d74ba7d5fd900e6af
Pavel Solomatin <crave_ozer_man@mail.ru>
2021-05-05 19:58:19 +0300

    05.05.2021 Много классов

--- core/src/com/sosungersteam/triggertrap/model/GameController.java
+++ core/src/com/sosungersteam/triggertrap/model/GameController.java
@@ -5,18 +5,24 @@ import com.sosungersteam.triggertrap.controller.Player;
 import com.sosungersteam.triggertrap.model.map.Door;
 import com.sosungersteam.triggertrap.model.map.Edge;
 import com.sosungersteam.triggertrap.model.map.Room;
+import com.sosungersteam.triggertrap.persons.Person;
 import com.sosungersteam.triggertrap.view.Renderer;
 
 public class GameController {
     private GameController gameController = null;
+
+    public static float SCALE = 1/16f;
 
     public Renderer renderer;
     public Player player;
     public Array<Door> doors;
     public Array<Edge> edges;
     public Array<Room> rooms;
+    public Array<Person> people;
 
-    private GameController() {}
+    private GameController() {
+
+    }
 
     GameController get() {
         if (gameController == null)
--- core/src/com/sosungersteam/triggertrap/persons/Person.java
+++ core/src/com/sosungersteam/triggertrap/persons/Person.java
@@ -6,9 +6,10 @@ import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.math.Rectangle;
 import com.badlogic.gdx.physics.box2d.Body;
 import com.badlogic.gdx.physics.box2d.Box2D;
+import com.badlogic.gdx.scenes.scene2d.Actor;
 import com.badlogic.gdx.utils.Array;
 
-public class Person {
+public class Person extends Actor {
     Texture texture;
     Body body;
     Rectangle size;
