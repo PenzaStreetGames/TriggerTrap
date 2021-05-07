fb31251319bdedae0df1fba55c57de3dffbda3ea
Pavel Solomatin <crave_ozer_man@mail.ru>
2021-05-07 16:11:26 +0300

    07.05.2021 Мусорка, очень чувствительная

--- core/src/com/sosungersteam/triggertrap/model/map/Bin.java
+++ core/src/com/sosungersteam/triggertrap/model/map/Bin.java
@@ -7,18 +7,26 @@ import com.sosungersteam.triggertrap.controller.Player;
 import com.sosungersteam.triggertrap.model.GameController;
 import com.sosungersteam.triggertrap.view.Renderer;
 
-public class Bin extends InteractiveObjects {
-    public Bin(World world, TiledMap map, Rectangle bounds) {
+public class Bin extends InteractiveObject {
+    public Bin(World world, TiledMap map, Rectangle bounds, String name) {
         super(world, map, bounds);
         fixture.setUserData(this);
+        this.name = name;
     }
 
     @Override
     public void onAttach() {
         Renderer.get().UI.buttonMap.get(Player.Buttons.ACT).setColor(1,1,1,0.75f);
+        GameController.get().player.setTargetObject(this);
     }
     @Override
     public void onDetach(){
         Renderer.get().UI.buttonMap.get(Player.Buttons.ACT).setColor(1,1,1,0.45f);
+        GameController.get().player.setTargetObject(null);
+    }
+
+    @Override
+    public void act() {
+        System.out.println(name);
     }
 }
--- core/src/com/sosungersteam/triggertrap/model/map/InteractiveObject.java
+++ core/src/com/sosungersteam/triggertrap/model/map/InteractiveObject.java
@@ -18,10 +18,11 @@ public abstract class InteractiveObject {
     protected Body body;
     protected Fixture fixture;
     public String name;
-    public InteractiveObject(World world, TiledMap map, Rectangle bounds){
+
+    public InteractiveObject(World world, TiledMap map, Rectangle bounds, boolean isSensor){
         this.world = world;
-        this.map=map;
-        this.bounds=bounds;
+        this.map = map;
+        this.bounds = bounds;
         BodyDef bdef = new BodyDef();
         PolygonShape shape = new PolygonShape();
         FixtureDef fdef = new FixtureDef();
@@ -30,8 +31,31 @@ public abstract class InteractiveObject {
         body = world.createBody(bdef);
         shape.setAsBox(bounds.getWidth()/2/1/16f,bounds.getHeight()/2/1/16f);///
         fdef.shape=shape;
+        fdef.isSensor = isSensor;
         fixture = body.createFixture(fdef);
     }
+    // Todo: разобраться с этим
+    public InteractiveObject(World world, TiledMap map, Rectangle bounds){
+        this.world = world;
+        this.map = map;
+        this.bounds = bounds;
+        BodyDef bdef = new BodyDef();
+        PolygonShape shape = new PolygonShape();
+        FixtureDef fdef = new FixtureDef();
+        bdef.type = BodyDef.BodyType.StaticBody;
+        bdef.position.set((bounds.getX()+bounds.getWidth()/2)/1/16f,(bounds.getY()+bounds.getHeight()/2)/1/16f);//
+        body = world.createBody(bdef);
+        shape.setAsBox(bounds.getWidth()/2/1/16f,bounds.getHeight()/2/1/16f);///
+        fdef.shape=shape;
+        fdef.isSensor = false;
+        fixture = body.createFixture(fdef);
+    }
+
+    public void setSensor(Rectangle rect, FixtureDef fdef) {
+        PolygonShape polygonShape = new PolygonShape();
+    }
+
     public abstract void onAttach();
     public abstract void onDetach();
+    public abstract void act();
 }
